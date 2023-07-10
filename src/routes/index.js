b9e5f9efa32b467a97197799bf7bcd06018dabba
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-10 08:05:57 +0700

    modify route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,17 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { /*Navigate,*/ Route, Routes } from "react-router-dom";
 import GuardRoute from "../components/GuardRoute";
 import GuestOnlyRoute from "../components/GuestOnlyRoute";
 
 import Login from "../pages/signin";
 import Register from "../pages/signup";
-import { HomeRoute } from "./HomeRoute";
-import { CategoriesRoute } from "./CategoriesRoute";
-import { TalentsRoute } from "./TalentsRoute";
-import { PaymentsRoute } from "./PaymentsRoute";
-import { EventsRoute } from "./EventsRoute";
-import { OrdersRoute } from "./OrdersRoute";
 import { LibraryRoute } from "./LibraryRoute";
-import PNavbar from "../components/Navbar";
+import { OTPRoute } from "./EnterOtpRoute";
+import { MyLearningRoute } from "./MyLearingRoute";
+// import { WishlistRoute } from "./WishlistRoute";
+// import { ChartRoute } from "./ChartRoute";
+import { DetailCourseRoute } from "./DetailCourseRoute";
+
+// import PNavbar from "../components/Navbar";
 
 export function AppRoutes() {
   return (
@@ -19,31 +19,38 @@ export function AppRoutes() {
       <Route
         path="/signin"
         element={
-          // <GuestOnlyRoute>
-          <Login />
-          // </GuestOnlyRoute>
+          <GuestOnlyRoute>
+            <Login />
+          </GuestOnlyRoute>
         }
       />
-      <Route path="/register" element={<Register />} />
-
       <Route
-        path="/"
+        path="/signup"
         element={
-          <>
-            <PNavbar />
-            {/* <GuardRoute /> */}
-          </>
+          <GuestOnlyRoute>
+            <Register />
+          </GuestOnlyRoute>
         }
-      >
-        <Route path="dashboard/*" element={<HomeRoute />} />
-        <Route path="categories/*" element={<CategoriesRoute />} />
-        <Route path="talents/*" element={<TalentsRoute />} />
-        <Route path="payments/*" element={<PaymentsRoute />} />
-        <Route path="events/*" element={<EventsRoute />} />
-        <Route path="orders/*" element={<OrdersRoute />} />
-        <Route path="library/*" element={<LibraryRoute />} />
-        <Route path="/" element={<Navigate to="/dashboard" replace={true} />} />
-      </Route>
+      />
+      <Route path="/library/*" element={<LibraryRoute />} />
+      <Route
+        path="/enter-otp"
+        element={
+          <GuestOnlyRoute>
+            <OTPRoute />
+          </GuestOnlyRoute>
+        }
+      />
+      <Route
+        path="/my-learning/*"
+        element={
+          <GuardRoute>
+            <MyLearningRoute />
+          </GuardRoute>
+        }
+      />
+
+      <Route path="/course/*" element={<DetailCourseRoute />} />
     </Routes>
   );
 }
