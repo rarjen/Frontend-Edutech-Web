543371c038ac08eaa88c91f641148d7609bee482
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-10 08:08:25 +0700

    make constants redux for product

---
+++ src/redux/products/constants.js
@@ -0,0 +1,5 @@
+export const START_FETCHING_PRODUCTS = "redux/products/START_FETCHING_PRODUCTS";
+export const SUCCESS_FETCHING_PRODUCTS =
+  "redux/products/SUCCESS_FETCHING_PRODUCTS";
+export const ERROR_FETCHING_PRODUCTS = "redux/products/ERROR_FETCHING_PRODUCTS";
+export const SET_KEYWORD = "redux/products/SET_KEYWORD";
